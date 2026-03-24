8563328a85b2531b2f75915398c7d7c20fdc3bb4
NkeleMosia <mosiankele39@gmail.com>
2026-03-24 11:08:43 +0200

    Initial commit - RunConnect Angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ClubCardComponent } from './components/club-card/club-card.component';
+import { SearchFiltersComponent } from './components/search-filters/search-filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    ClubCardComponent,
+    SearchFiltersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/club-card/club-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-club-card',
+  templateUrl: './club-card.component.html',
+  styleUrls: ['./club-card.component.css']
+})
+export class ClubCardComponent {
+
+}
